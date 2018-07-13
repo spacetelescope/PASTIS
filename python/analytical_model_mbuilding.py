336c5d0fd11976e7f3d50069cb8c3155b5ca5b71
Iva Laginja <ilaginja@stsci.edu>
2018-07-13 15:14:08 -0400

    Separated PASTIS matrix building from contrast calculation/comparison.

--- python/analytical_model_mbuilding.py
+++ python/analytical_model_mbuilding.py
@@ -1,15 +1,12 @@
-"""This program constructs the matrix M for PASTIS and compares the contrast of PASTIS with image generation with
-the contrast from matrix PASTIS."""
+"""This program constructs the matrix M for PASTIS and aves it."""
 
 import os
 import time
 import numpy as np
-import matplotlib.pyplot as plt
 
 from python.config import CONFIG_INI
 import python.util_pastis as util
 import python.analytical_model as am
-import python.webbpsf_imaging as webbim
 
 
 if __name__ == '__main__':
@@ -24,7 +21,7 @@ if __name__ == '__main__':
     tel_size_px = CONFIG_INI.getint('numerical', 'tel_size_px')
     im_size = CONFIG_INI.getint('numerical', 'im_size_px')
     largeur = tel_size_px * sampling
-    fpm = CONFIG_INI.get('coronagraph', 'focal_plane_mask')                 # focal plane mask
+    fpm = CONFIG_INI.get('coronagraph', 'focal_plane_mask')         # focal plane mask
     lyot_stop = CONFIG_INI.get('coronagraph', 'pupil_plane_stop')   # Lyot stop
     filter = CONFIG_INI.get('filter', 'name')
     inner_wa = CONFIG_INI.getint('coronagraph', 'IWA')
@@ -60,47 +57,11 @@ if __name__ == '__main__':
                 matrix_pastis[i,j] = (matrix_two_N[i,j] - matrix_two_N[i,i] - matrix_two_N[j,j]) / 2.
 
     # Save matrix to file
-        filename = 'PASTISmatrix_' + zern_mode.name + '_' + zern_mode.convention + str(zern_mode.index)
-        util.write_fits(matrix_pastis, os.path.join(dataDir, 'results', filename + '.fits'), header=None, metadata=None)
+    filename = 'PASTISmatrix_' + zern_mode.name + '_' + zern_mode.convention + str(zern_mode.index)
+    util.write_fits(matrix_pastis, os.path.join(dataDir, 'results', filename + '.fits'), header=None, metadata=None)
 
-
-
-    #-# Compare PASTIS contrast from PSF generation and PASTIS contrast from matrix PASTIS
-    # Create random aberration coefficients
-    Aber = np.random.random([nb_seg]) * 10   # piston values
-
-    # mean subtraction for piston
-    if zern_number == 1:
-        Aber -= np.mean(Aber)
-
-    ### WEBBPSF
-    # Set up NIRCam and coronagraph
-    psf_webbpsf = webbim.nircam_coro(filter, fpm, lyot_stop)
-    # Get the mean conrast from the WebbPSF coronagraph
-    contrast_webbpsf = np.mean(psf_webbpsf[np.where(psf_webbpsf != 0)])
-
-    ### IMAGE PASTIS
-    # Create calibrated image from analytical model
-    dh_psf = am.analytical_model(zern_number, Aber, cali=True)
-    # Get the mean contrast from image PASTIS
-    contrast_am = np.mean(dh_psf[np.where(dh_psf != 0)])
-
-    # Load in baseline contast
-    contrastname = 'base-contrast_' + zern_mode.name + '_' + zern_mode.convention + str(zern_mode.index)
-    contrast_base = float(np.loadtxt(os.path.join(dataDir, 'calibration', contrastname+'.txt')))
-
-    ### MATRIX PASTIS
-    # Get mean contrast from matrix PASTIS
-    contrast_matrix = util.pastis_contrast(Aber, matrix_pastis) + contrast_base   # calculating contrast with PASTIS matrix model
-    ratio = contrast_am / contrast_matrix
-
-    print('Mean contrast from WebbPSF:', contrast_webbpsf)
-    print('Mean contrast with image PASTIS:', contrast_am)
-    print('Contrast from matrix PASTIS:', contrast_matrix)
-    print('Ratio image PASTIS / matrix PASTIS:', ratio)
+    print('Matrix saved to:', os.path.join(dataDir, 'results', filename + '.fits'))
 
     # Tell us how long it took to finish.
     end_time = time.time()
     print('Runtime for analytical_model_mbuild.py:', end_time - start_time, 'sec =', (end_time - start_time) / 60, 'min')
-
-
--- python/webbpsf_imaging.py
+++ python/webbpsf_imaging.py
@@ -1,16 +1,25 @@
 """
 This is a module containing functions to create JWST coronagraphic images.
 """
+import  os
 import webbpsf
 from python.config import CONFIG_INI
 
+# Setting to ensure that PyCharm finds the webbpsf-data folder. If you don't know where it is, find it with:
+# webbpsf.utils.get_webbpsf_data_path()
+# --> e.g.: >>source activate astroconda   >>ipython   >>import webbpsf   >>webbpsf.utils.get_webbpsf_data_path()
+os.environ['WEBBPSF_PATH'] = CONFIG_INI.get('local', 'webbpsf_data_path')
+
+
+nb_seg = CONFIG_INI.getint('telescope', 'nb_subapertures')
+wss_segs = webbpsf.constants.SEGNAMES_WSS_ORDER
 im_size = CONFIG_INI.getint('numerical', 'im_size_px')
 fpm = CONFIG_INI.get('coronagraph', 'focal_plane_mask')  # focal plane mask
 lyot_stop = CONFIG_INI.get('coronagraph', 'pupil_plane_stop')  # Lyot stop
 filter = CONFIG_INI.get('filter', 'name')
 
 
-def nircam_coro(filter, fpm, ppm):
+def nircam_coro(filter, fpm, ppm, Aber_WSS):
 
     # Set up NIRCam and coronagraph
     nc = webbpsf.NIRCam()
@@ -18,6 +27,12 @@ def nircam_coro(filter, fpm, ppm):
     nc.filter = filter
     nc.image_mask = fpm
     nc.pupil_mask = ppm
+
+    # Adjust OTE with aberrations
+    nc, ote = webbpsf.enable_adjustable_ote(nc)
+    for i in range(nb_seg):
+        seg = wss_segs[i].split('-')[0]
+        ote._apply_hexikes_to_seg(seg, Aber_WSS[i,:])
 
     # Calculate PSF
     psf_nc = nc.calc_psf(fov_pixels=int(im_size), nlambda=1)
