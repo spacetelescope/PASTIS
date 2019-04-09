d50cb1da4d6adb6ba9de17f0b06e33140712424b
Iva Laginja <ilaginja@stsci.edu>
2019-04-09 10:56:00 -0400

    Changed contrast saving in matrix generations to scientific notation.

--- pastis/matrix_building_analytical.py
+++ pastis/matrix_building_analytical.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
     # Save the PSF and DH image *cubes* as well (as opposed to each one individually)
     util.write_fits(all_ims, os.path.join(resDir, 'psfs', 'psf_cube' + '.fits'), header=None, metadata=None)
     util.write_fits(all_dhs, os.path.join(resDir, 'darkholes', 'dh_cube' + '.fits'), header=None, metadata=None)
-    np.savetxt(os.path.join(resDir, 'contrasts.txt'), all_contrasts, fmt='%2.2f')
+    np.savetxt(os.path.join(resDir, 'contrasts.txt'), all_contrasts, fmt='%e')
 
     # Tell us how long it took to finish.
     end_time = time.time()
