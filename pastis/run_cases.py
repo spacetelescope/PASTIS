7e4a03ad5975ab993510c533f341caa211fd74f2
Iva Laginja <laginja.iva@gmail.com>
2020-01-14 09:03:52 -0500

    Add import os and fix data path

--- pastis/run_cases.py
+++ pastis/run_cases.py
@@ -1,7 +1,7 @@
 """
 Run different cases, free combination of matrices and analysis scripts.
 """
-
+import os
 from hockeystick_contrast_curve import hockeystick_luvoir
 from matrix_building_numerical import num_matrix_luvoir
 from modal_analysis import run_full_pastis_analysis_luvoir
@@ -16,16 +16,19 @@ if __name__ == '__main__':
     dir_large = num_matrix_luvoir(design='large')
     
     # Then generate all hockeystick curves
+    #dir_small = '/Users/ilaginja/Documents/data_from_repos/pastis_data/2020-01-13T22-05-24_luvoir-small'
     result_dir_small = os.path.join(dir_small, 'results')
     matrix_dir_small = os.path.join(dir_small, 'matrix_numerical')
     hockeystick_luvoir(apodizer_choice='small', matrixdir=matrix_dir_small, resultdir=result_dir_small, range_points=50, no_realizations=20)
-    
-    result_dir_medium = os.path.join(dir_medium, run_choice, 'results')
-    matrix_dir_medium = os.path.join(dir_medium, run_choice, 'matrix_numerical')
+
+    #dir_medium = '/Users/ilaginja/Documents/data_from_repos/pastis_data/2020-01-14T00-19-14_luvoir-medium'
+    result_dir_medium = os.path.join(dir_medium, 'results')
+    matrix_dir_medium = os.path.join(dir_medium, 'matrix_numerical')
     hockeystick_luvoir(apodizer_choice='medium', matrixdir=matrix_dir_medium, resultdir=result_dir_medium, range_points=50, no_realizations=20)
-    
-    result_dir_large = os.path.join(dir_large, run_choice, 'results')
-    matrix_dir_large = os.path.join(dir_large, run_choice, 'matrix_numerical')
+
+    #dir_large = '/Users/ilaginja/Documents/data_from_repos/pastis_data/2020-01-14T02-40-04_luvoir-large'
+    result_dir_large = os.path.join(dir_large, 'results')
+    matrix_dir_large = os.path.join(dir_large, 'matrix_numerical')
     hockeystick_luvoir(apodizer_choice='large', matrixdir=matrix_dir_large, resultdir=result_dir_large, range_points=50, no_realizations=20)
     
     # Finally run full analysis on all three cases
