cd740c33ae1d1d5bf8d8dd73e86a1c41911dc428
Iva Laginja <laginja.iva@gmail.com>
2020-02-17 00:14:36 -0500

    Add option for single-mode error budget

--- pastis/optimized_error_budget.py
+++ pastis/optimized_error_budget.py
@@ -1,9 +1,14 @@
 """
-This script calculates the sigmas and cumulative contrast plot of the optimized error budget, coming form the mu map.
-We also save the segment-space and mode-space covariance matrices.
+This script calculates the sigmas and cumulative contrast plot of different mode-based error budgets.
+The "single-mode error budget" is where one mode alone accounts for the entire contrast contribution on top on the
+coronagraph floor to reach the target contrast.
+The "optimized error" budget comes form the mu map and we also save the segment-space and mode-space covariance matrices.
+
+The flat error budget gets calculated as a standard part of the main PASTIS analysis in "modal_analysis.py".
 """
 
 import os
+import astropy.units as u
 import numpy as np
 import hcipy as hc
 import matplotlib.pyplot as plt
@@ -14,7 +19,41 @@ from modal_analysis import modes_from_file, cumulative_contrast_e2e
 from e2e_simulators.luvoir_imaging import LuvoirAPLC
 
 
-def build_optimized_error_budget(design, run_choice, c_target):
+def single_mode_sigma(c_target, c_floor, evalue):
+    """
+    Calculate the mode weight sigma assuming a single-mode error budget.
+    :param c_target: float, overall target contrast
+    :param c_floor: float, coronagraph contrast floor
+    :param evalue: float, PASTIS eigenvalue of the mode weight to be calcualted
+    :return:
+    """
+    sigma = np.sqrt((c_target - c_floor) / evalue)
+    return sigma
+
+
+def single_mode_contrasts(sigma, pmodes, single_mode, luvoir):
+
+        # Calculate the OPD from scaling the  mode by sigma
+        opd = pmodes[:, single_mode - 1] * sigma
+
+        # Put OPD on LUVOIR simulator
+        luvoir.flatten()
+        for seg, val in enumerate(opd):
+            val *= u.nm
+            luvoir.set_segment(seg + 1, val.to(u.m).value / 2, 0, 0)
+
+        # Get PSF from putting this OPD on the SM
+        psf, ref = luvoir.calc_psf(ref=True)
+        norm = ref.max()
+
+        # Calculate the contrast from that PSF
+        dh_intensity = psf / norm * luvoir.dh_mask
+        contrast = np.mean(dh_intensity[np.where(luvoir.dh_mask != 0)])
+
+        return contrast
+
+
+def build_mode_based_error_budget(design, run_choice, c_target, error_budget='optimized', single_mode=73):
 
     # Data directory
     workdir = os.path.join(CONFIG_INI.get('local', 'local_data_path'), run_choice)
@@ -26,87 +65,124 @@ def build_optimized_error_budget(design, run_choice, c_target):
     optics_input = CONFIG_INI.get('LUVOIR', 'optics_path')
     sampling = CONFIG_INI.getfloat('numerical', 'sampling')
     luvoir = LuvoirAPLC(optics_input, design, sampling)
-
-    # Generate reference PSF and coronagraph baseline
     luvoir.flatten()
-
-    # Make dark hole mask
-    dh_outer = hc.circular_aperture(2 * luvoir.apod_dict[design]['owa'] * luvoir.lam_over_d)(luvoir.focal_det)
-    dh_inner = hc.circular_aperture(2 * luvoir.apod_dict[design]['iwa'] * luvoir.lam_over_d)(luvoir.focal_det)
-    dh_mask = (dh_outer - dh_inner).astype('bool')
 
     # Load PASTIS modes and eigenvalues
     pmodes, svals = modes_from_file(workdir)
 
-    # Load the mu map
-    mus = np.loadtxt(os.path.join(workdir, 'results', 'mus_{}.txt'.format(c_target)))
+    if error_budget == 'single_mode':
 
-    # Build segment-space covariance matrix Cy
-    cy = np.diag(np.square(mus))
-    hc.write_fits(cy, os.path.join(workdir, 'results', 'cy_{}.fits'.format(c_target)))
+        # Generate baseline contrast
+        psf_unaber, ref = luvoir.calc_psf(ref=True)
+        norm = ref.max()
+        dh_intensity = psf_unaber / norm * luvoir.dh_mask
+        baseline_contrast = np.mean(dh_intensity[np.where(dh_intensity != 0)])
+        print('baseline_contrast: {}'.format(baseline_contrast))
 
-    # Calculate mode-space covariance matrix Cx
-    cx = np.dot(np.transpose(pmodes), np.dot(cy, pmodes))
-    hc.write_fits(cx, os.path.join(workdir, 'results', 'cx_{}.fits'.format(c_target)))
+        # Calculate the mode weight
+        single_sigma = single_mode_sigma(c_target, baseline_contrast, svals[single_mode-1])
+        print(svals[single_mode-1])
+        print('single_sigma: {}'.format(single_sigma))
 
-    # Extract optimized mode weights
-    sigmas_opt = np.sqrt(np.diag(cx))
-    np.savetxt(os.path.join(workdir, 'results', 'sigmas_opt_{}.txt'.format(c_target)), sigmas_opt)
+        single_contrast = single_mode_contrasts(single_sigma, pmodes, single_mode, luvoir)
+        print('contrast: {}'.format(single_contrast))
 
-    # Calculate contast per mode
-    per_mode_opt_e2e = cumulative_contrast_e2e(pmodes, sigmas_opt, luvoir, dh_mask, individual=True)
-    np.savetxt(os.path.join(workdir, 'results', 'per-mode_contrast_optimized_e2e_{}.txt'.format(c_target)),
-               per_mode_opt_e2e)
+        # Make array of target contrasts
+        c_list = [5e-11, 8e-11, 1e-10, 5e-10, 1e-9, 5e-9, 1e-8]
+        sigma_list = []
 
-    # Calculate optimized cumulative contrast plot
-    cumulative_opt_e2e = cumulative_contrast_e2e(pmodes, sigmas_opt, luvoir, dh_mask)
-    np.savetxt(os.path.join(workdir, 'results', 'cumulative_contrast_optimized_e2e_{}.txt'.format(c_target)),
-               cumulative_opt_e2e)
+        # Calculate according sigmas
+        for i, con in enumerate(c_list):
+            sigma_list.append(single_mode_sigma(con, baseline_contrast, svals[single_mode-1]))
 
-    ### Plotting
+        # Calculate recovered contrasts
+        c_recov = []
+        for i, sig in enumerate(sigma_list):
+            c_recov.append(single_mode_contrasts(sig, pmodes, single_mode, luvoir))
 
-    # Optimized mode weights
-    plt.figure()
-    plt.plot(sigmas_opt)
-    plt.semilogy()
-    plt.title('Constraints per mode', size=15)
-    plt.xlabel('Mode', size=15)
-    plt.ylabel('Max mode contribution $\sigma_p$ (nm)', size=15)
-    plt.savefig(os.path.join(workdir, 'results', 'sigmas_opt_{}.pdf'.format(c_target)))
+        print('c_recov: {}'.format(c_recov))
+        np.savetxt(os.path.join(workdir, 'results', 'single_mode_targets.txt'), c_list)
+        np.savetxt(os.path.join(workdir, 'results', 'single_mode_recovered_mode{}.txt'.format(single_mode)), c_recov)
 
-    # Segment-space covariance matrix
-    plt.figure(figsize=(10, 10))
-    plt.imshow(cy)
-    plt.title('Segment-space covariance matrix $C_y$', size=20)
-    plt.xlabel('segments', size=20)
-    plt.ylabel('segments', size=20)
-    plt.colorbar()
-    plt.savefig(os.path.join(workdir, 'results', 'cy_{}.pdf'.format(c_target)))
+        plt.plot(c_list, c_recov)
+        plt.title('Single-mode scaling')
+        plt.semilogy()
+        plt.semilogx()
+        plt.xlabel('Target contrast $c_{target}$')
+        plt.ylabel('Recovered contrast')
+        plt.savefig(os.path.join(workdir, 'results', 'single_mode_scaled_mode{}.pdf'.format(single_mode)))
 
-    # Mode-space covariance matrix
-    plt.figure(figsize=(10, 10))
-    plt.imshow(cx)
-    plt.title('Mode-space covariance matrix $C_x$', size=20)
-    plt.xlabel('modes', size=20)
-    plt.ylabel('modes', size=20)
-    plt.colorbar()
-    plt.savefig(os.path.join(workdir, 'results', 'cx_{}.pdf'.format(c_target)))
+    if error_budget == 'optimized':
 
-    # Contrast per mode from E2E simulator for optimized error budget
-    plt.figure(figsize=(16, 10))
-    plt.plot(per_mode_opt_e2e)
-    plt.title('Optimized E2E contrast per mode for target $c$ = {}'.format(c_target), size=15)
-    plt.xlabel('Mode number', size=15)
-    plt.ylabel('Contrast', size=15)
-    plt.savefig(os.path.join(workdir, 'results', 'per-mode_contrast_plot_optimized_{}.pdf'.format(c_target)))
+        # Load the mu map
+        mus = np.loadtxt(os.path.join(workdir, 'results', 'mus_{}.txt'.format(c_target)))
 
-    # Cumulative contrast from E2E simulator for optimized error budget
-    plt.figure(figsize=(16, 10))
-    plt.plot(cumulative_opt_e2e)
-    plt.title('Optimized E2E cumulative contrast for target $c$ = {}'.format(c_target), size=15)
-    plt.xlabel('Mode number', size=15)
-    plt.ylabel('Contrast', size=15)
-    plt.savefig(os.path.join(workdir, 'results', 'cumulative_contrast_plot_optimized_{}.pdf'.format(c_target)))
+        # Build segment-space covariance matrix Cy
+        cy = np.diag(np.square(mus))
+        hc.write_fits(cy, os.path.join(workdir, 'results', 'cy_{}.fits'.format(c_target)))
+
+        # Calculate mode-space covariance matrix Cx
+        cx = np.dot(np.transpose(pmodes), np.dot(cy, pmodes))
+        hc.write_fits(cx, os.path.join(workdir, 'results', 'cx_{}.fits'.format(c_target)))
+
+        # Extract optimized mode weights
+        sigmas_opt = np.sqrt(np.diag(cx))
+        np.savetxt(os.path.join(workdir, 'results', 'sigmas_opt_{}.txt'.format(c_target)), sigmas_opt)
+
+        # Calculate contrast per mode
+        per_mode_opt_e2e = cumulative_contrast_e2e(pmodes, sigmas_opt, luvoir, luvoir.dh_mask, individual=True)
+        np.savetxt(os.path.join(workdir, 'results', 'per-mode_contrast_optimized_e2e_{}.txt'.format(c_target)),
+                   per_mode_opt_e2e)
+
+        # Calculate optimized cumulative contrast plot
+        cumulative_opt_e2e = cumulative_contrast_e2e(pmodes, sigmas_opt, luvoir, luvoir.dh_mask)
+        np.savetxt(os.path.join(workdir, 'results', 'cumulative_contrast_optimized_e2e_{}.txt'.format(c_target)),
+                   cumulative_opt_e2e)
+
+        ### Plotting
+
+        # Optimized mode weights
+        plt.figure()
+        plt.plot(sigmas_opt)
+        plt.semilogy()
+        plt.title('Constraints per mode', size=15)
+        plt.xlabel('Mode', size=15)
+        plt.ylabel('Max mode contribution $\sigma_p$ (nm)', size=15)
+        plt.savefig(os.path.join(workdir, 'results', 'sigmas_opt_{}.pdf'.format(c_target)))
+
+        # Segment-space covariance matrix
+        plt.figure(figsize=(10, 10))
+        plt.imshow(cy)
+        plt.title('Segment-space covariance matrix $C_y$', size=20)
+        plt.xlabel('segments', size=20)
+        plt.ylabel('segments', size=20)
+        plt.colorbar()
+        plt.savefig(os.path.join(workdir, 'results', 'cy_{}.pdf'.format(c_target)))
+
+        # Mode-space covariance matrix
+        plt.figure(figsize=(10, 10))
+        plt.imshow(cx)
+        plt.title('Mode-space covariance matrix $C_x$', size=20)
+        plt.xlabel('modes', size=20)
+        plt.ylabel('modes', size=20)
+        plt.colorbar()
+        plt.savefig(os.path.join(workdir, 'results', 'cx_{}.pdf'.format(c_target)))
+
+        # Contrast per mode from E2E simulator for optimized error budget
+        plt.figure(figsize=(16, 10))
+        plt.plot(per_mode_opt_e2e)
+        plt.title('Optimized E2E contrast per mode for target $c$ = {}'.format(c_target), size=15)
+        plt.xlabel('Mode number', size=15)
+        plt.ylabel('Contrast', size=15)
+        plt.savefig(os.path.join(workdir, 'results', 'per-mode_contrast_plot_optimized_{}.pdf'.format(c_target)))
+
+        # Cumulative contrast from E2E simulator for optimized error budget
+        plt.figure(figsize=(16, 10))
+        plt.plot(cumulative_opt_e2e)
+        plt.title('Optimized E2E cumulative contrast for target $c$ = {}'.format(c_target), size=15)
+        plt.xlabel('Mode number', size=15)
+        plt.ylabel('Contrast', size=15)
+        plt.savefig(os.path.join(workdir, 'results', 'cumulative_contrast_plot_optimized_{}.pdf'.format(c_target)))
 
 
 if __name__ == '__main__':
@@ -115,4 +191,4 @@ if __name__ == '__main__':
     run = CONFIG_INI.get('numerical', 'current_analysis')
     c_stat = 1e-10
 
-    build_optimized_error_budget(coro_design, run, c_stat)
+    build_mode_based_error_budget(coro_design, run, c_stat, error_budget='single_mode', single_mode=73)
