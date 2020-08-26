b49aaadbe7c95c26b1812921eca3f53a7882c9c2
Iva Laginja <laginja.iva@gmail.com>
2020-08-26 12:21:43 +0200

    update luvoir launcher with instrument keyword

--- pastis/launchers/run_luvoir.py
+++ pastis/launchers/run_luvoir.py
@@ -1,5 +1,6 @@
 """
-Run different cases, free combination of matrices and analysis scripts.
+Launcher script to start a full LUVOIR run: combinations of matrix generation, hockey stick curve and PASTIS analysis,
+freely choosable between the small, medium and large coronagraph designs.
 """
 import os
 
@@ -30,17 +31,17 @@ if __name__ == '__main__':
     # Then generate all hockeystick curves
     result_dir_small = os.path.join(dir_small, 'results')
     matrix_dir_small = os.path.join(dir_small, 'matrix_numerical')
-    hockeystick_curve(instrument='LUVOIR', apodizer_choice='small', matrixdir=matrix_dir_small, resultdir=result_dir_small, range_points=10, no_realizations=2)
+    hockeystick_curve(instrument='LUVOIR', apodizer_choice='small', matrixdir=matrix_dir_small, resultdir=result_dir_small, range_points=50, no_realizations=20)
 
     #result_dir_medium = os.path.join(dir_medium, 'results')
     #matrix_dir_medium = os.path.join(dir_medium, 'matrix_numerical')
-    #hockeystick_luvoir(apodizer_choice='medium', matrixdir=matrix_dir_medium, resultdir=result_dir_medium, range_points=50, no_realizations=20)
+    #hockeystick_curve(instrument='LUVOIR', apodizer_choice='medium', matrixdir=matrix_dir_medium, resultdir=result_dir_medium, range_points=50, no_realizations=20)
 
     #result_dir_large = os.path.join(dir_large, 'results')
     #matrix_dir_large = os.path.join(dir_large, 'matrix_numerical')
-    #hockeystick_luvoir(apodizer_choice='large', matrixdir=matrix_dir_large, resultdir=result_dir_large, range_points=50, no_realizations=20)
+    #hockeystick_curve(instrument='LUVOIR', apodizer_choice='large', matrixdir=matrix_dir_large, resultdir=result_dir_large, range_points=50, no_realizations=20)
     
     # Finally run full analysis on all three cases
-    run_full_pastis_analysis_luvoir(design='small', run_choice=dir_small)
-    #run_full_pastis_analysis_luvoir(design='medium', run_choice=dir_medium)
-    #run_full_pastis_analysis_luvoir(design='large', run_choice=dir_large)
+    run_full_pastis_analysis_luvoir(instrument='LUVOIR', design='small', run_choice=dir_small)
+    #run_full_pastis_analysis_luvoir(instrument='LUVOIR', design='medium', run_choice=dir_medium)
+    #run_full_pastis_analysis_luvoir(instrument='LUVOIR', design='large', run_choice=dir_large)
