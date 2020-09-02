f7ca2fec7f0bdd0c31bc104bcbcd90bc4e458642
Iva Laginja <laginja.iva@gmail.com>
2020-09-02 13:17:23 +0200

    lower default numbers for hockey stick curve in launcher scripts

--- pastis/launchers/run_luvoir.py
+++ pastis/launchers/run_luvoir.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     # Then generate all hockeystick curves
     result_dir_small = os.path.join(dir_small, 'results')
     matrix_dir_small = os.path.join(dir_small, 'matrix_numerical')
-    hockeystick_curve(instrument='LUVOIR', apodizer_choice='small', matrixdir=matrix_dir_small, resultdir=result_dir_small, range_points=50, no_realizations=20)
+    hockeystick_curve(instrument='LUVOIR', apodizer_choice='small', matrixdir=matrix_dir_small, resultdir=result_dir_small, range_points=10, no_realizations=3)
 
     #result_dir_medium = os.path.join(dir_medium, 'results')
     #matrix_dir_medium = os.path.join(dir_medium, 'matrix_numerical')
