f11478ea070c64f95b00d6de72e82ac7c1e0c3c9
Iva Laginja <laginja.iva@gmail.com>
2020-09-14 16:43:20 +0200

    add a setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(name='pastis',
+      version='2.1.0',
+      url='https://github.com/spacetelescope/pastis',
+      license='BSD 3-Clause',
+      packages=find_packages())
