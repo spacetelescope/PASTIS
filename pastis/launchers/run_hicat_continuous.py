ca043a2c4787e09b9ec1938036a29c46da19607c
Iva Laginja <laginja.iva@gmail.com>
2020-10-16 19:38:35 +0200

    activate full run

--- pastis/launchers/run_hicat_continuous.py
+++ pastis/launchers/run_hicat_continuous.py
@@ -13,10 +13,10 @@ import pastis.util as util
 if __name__ == '__main__':
 
     # Generate the matrix
-    #dir_run = num_matrix_multiprocess(instrument='HiCAT_continuous', savepsfs=False, saveopds=False)
+    dir_run = num_matrix_multiprocess(instrument='HiCAT_continuous', savepsfs=False, saveopds=False)
 
     # Alternatively, pick data location to run PASTIS analysis on
-    dir_run = os.path.join(CONFIG_PASTIS.get('local', 'local_data_path'), '2020-10-14T10-51-49_hicat_continuous')
+    #dir_run = os.path.join(CONFIG_PASTIS.get('local', 'local_data_path'), '2020-10-14T10-51-49_hicat_continuous')
 
     # Set up loggers for data analysis
     util.setup_pastis_logging(dir_run, 'pastis_analysis')
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     # Then generate hockeystick curve
     result_dir = os.path.join(dir_run, 'results')
     matrix_dir = os.path.join(dir_run, 'matrix_numerical')
-    hockeystick_curve(instrument='HiCAT_continuous', matrixdir=matrix_dir, resultdir=result_dir, range_points=10, no_realizations=3)
+    #hockeystick_curve(instrument='HiCAT_continuous', matrixdir=matrix_dir, resultdir=result_dir, range_points=10, no_realizations=3)
 
     # Finally run the analysis
     run_full_pastis_analysis(instrument='HiCAT_continuous', run_choice=dir_run, c_target=1e-7)
