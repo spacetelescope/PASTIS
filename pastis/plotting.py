fixed unit conversion when going to contrast/wave**2

pastis/plotting.py
@@ -32,7 +32,7 @@ def plot_pastis_matrix(pastis_matrix, wvln, out_dir, fname_suffix='', save=False
         fname += f'_{fname_suffix}'
 
     plt.figure(figsize=(10, 10))
-    plt.imshow(pastis_matrix / wvln**2)
+    plt.imshow(pastis_matrix * wvln**2)
     plt.title('Semi-analytical PASTIS matrix', size=30)
     plt.tick_params(axis='both', which='both', length=6, width=2, labelsize=25)
     cbar = plt.colorbar(fraction=0.046, pad=0.06)  # format='%.0e'
@@ -101,7 +101,7 @@ def plot_eigenvalues(eigenvalues, nseg, wvln, out_dir, fname_suffix='', save=Fal
         fname += f'_{fname_suffix}'
 
     plt.figure(figsize=(12, 8))
-    plt.plot(np.arange(1, nseg + 1), eigenvalues / wvln, linewidth=3, color='red')
+    plt.plot(np.arange(1, nseg + 1), eigenvalues * wvln**2, linewidth=3, color='red')
     plt.semilogy()
     plt.tick_params(axis='both', which='both', length=6, width=2, labelsize=30)
     plt.title('PASTIS matrix eigenvalues', size=30)