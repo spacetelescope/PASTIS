8dfdc13634f4b7290150edd6eedf4eda18b92e49
Iva Laginja <laginja.iva@gmail.com>
2020-12-29 01:22:25 +0100

    add first tests for pastis analysis

---
+++ pastis/tests/test_pastis_analysis.py
@@ -0,0 +1,65 @@
+import os
+from astropy.io import fits
+import numpy as np
+
+from pastis.config import CONFIG_PASTIS
+from pastis.matrix_building_numerical import calculate_unaberrated_contrast_and_normalization
+from pastis import pastis_analysis
+
+
+# Read the LUVOIR-A small APLC PASTIS matrix
+# TODO: This needs to be a matrix I will save with Git LFS
+matrix_path = os.path.join(CONFIG_PASTIS.get('local', 'local_data_path'), '2020-07-17T00-57-55_luvoir-small', 'matrix_numerical', 'PASTISmatrix_num_piston_Noll1.fits')
+#matrix_path = os.path.join('data', 'pastis_matrices', 'LUVOIR_small_matrix_piston-only.fits')
+
+LUVOIR_MATRIX_SMALL = fits.getdata(matrix_path)
+NSEG = LUVOIR_MATRIX_SMALL.shape[0]
+
+CORO_FLOOR, NORM, LUVOIR_SIM = calculate_unaberrated_contrast_and_normalization('LUVOIR', 'small')
+
+C_TARGET = 1e-10
+
+
+def test_modes_from_matrix():
+    # Calculate modes and eigenvalues from matrix
+    pmodes, svals, vh = np.linalg.svd(LUVOIR_MATRIX_SMALL, full_matrices=True)
+
+    # Check that all eigenvalues are positive
+    assert (svals > 0).all(), 'SVD yields negative eigenvalues'
+
+    # Test eigenvalue problem for a selection of modes, M u = lam u
+    test_modes = [3, 45, 98]
+    for mode_nb in test_modes:
+        left = np.dot(LUVOIR_MATRIX_SMALL, svals[mode_nb])
+        right = np.dot(LUVOIR_MATRIX_SMALL, pmodes[:, mode_nb])
+        assert np.allclose(left, right)
+
+
+def test_uniform_mode_weights():
+    # Calculate modes and eigenvalues from matrix
+    pmodes, svals, vh = np.linalg.svd(LUVOIR_MATRIX_SMALL, full_matrices=True)
+
+    # Calculate the mode weights for uniform contrast allocation
+    sigmas = pastis_analysis.calculate_sigma(C_TARGET, NSEG, svals, CORO_FLOOR)
+
+    # Calculate cumulative contrast from the sigmas, with PASTIS propagation
+    cumulative_pastis = pastis_analysis.cumulative_contrast_matrix(pmodes, sigmas, LUVOIR_MATRIX_SMALL, CORO_FLOOR)
+
+    # Calculate cumulative contrast from the sigmas, with E2E simulator
+    cumulative_e2e = pastis_analysis.cumulative_contrast_e2e('LUVOIR', pmodes, sigmas, LUVOIR_SIM, LUVOIR_SIM.dh_mask.shaped, NORM)
+
+    # Check that final calculated contrast is equal to target contrast
+    assert np.isclose(cumulative_pastis[-1], C_TARGET, rtol=1e-3, atol=1e-15), "Cumulative contrast with PASTIS doesn't end up at target contrast."
+    assert np.isclose(cumulative_e2e[-1], C_TARGET, rtol=1e-2, atol=1e-15), "Cumulative contrast with E2E doesn't end up at target contrast."
+
+    # Define a couple of testing points (mode indices)
+    test_modes_first = [4, 65, 111]
+    test_modes_second = [59, 109, 35]
+    test_intervals = [1, 30, 20]
+
+    # Test for equal contrast difference in equal intervals
+    for first, second, interval in zip(test_modes_first, test_modes_second, test_intervals):
+        assert np.isclose(cumulative_pastis[first] - cumulative_pastis[first - interval],
+                          cumulative_pastis[second] - cumulative_pastis[second - interval], rtol=1e-5, atol=1e-27)
+        assert np.isclose(cumulative_e2e[first] - cumulative_e2e[first - interval],
+                          cumulative_e2e[second] - cumulative_e2e[second - interval], rtol=1e-1, atol=1e-15)
