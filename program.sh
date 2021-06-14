2c152780a9e44b59a0dd5dfa9a71fcd47a6a4fa9
Marken <david.bourgeois@obspm.fr>
2021-06-14 16:03:00 +0200

    24x24

--- program.sh
+++ program.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -J full_DM_1n       # run's name
+#SBATCH -J DM_24x24         # run's name
 #SBATCH -N 1                   # request 1 node 
-#SBATCH -c 16                 # request 8 cpu per task
-#SBATCH --mem=16GB             # request 16GB
-#SBATCH -t 1:00:00            # request 6 hours walltime
+#SBATCH -c 24                 # request 8 cpu per task
+#SBATCH --mem=32GB             # request 16GB
+#SBATCH -t 4:00:00            # request 4 hours walltime
 #SBATCH -o Out.txt             # output file name
 #SBATCH -e Err.txt             # error file name
 #SBATCH --mail-type=BEGIN,END  # send me a mail at beginning and end of the job
