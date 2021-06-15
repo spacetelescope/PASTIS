cfc634438fe22598857d15674b1a9ae9b1b5faa1
David Bourgeois <david.bourgeois@obspm.fr>
2021-06-15 21:06:23 +0200

    1st modifs

--- pastis/launchers/run_rst.py
+++ pastis/launchers/run_rst.py
@@ -2,18 +2,30 @@
 Launcher script to start a full RST run: generate matrix and run full PASTIS analysis.
 """
 import os
+import logging
 
 from pastis.config import CONFIG_PASTIS
 from pastis.hockeystick_contrast_curve import hockeystick_curve
 from pastis.matrix_generation.matrix_building_numerical import MatrixIntensityRST
+from pastis.matrix_generation.matrix_from_efields import MatrixEfieldRST
 from pastis.pastis_analysis import run_full_pastis_analysis
 import pastis.util as util
 
 
+log = logging.getLogger()
+
 if __name__ == '__main__':
 
     # Generate the matrix
-    run_matrix = MatrixIntensityRST(initial_path=CONFIG_PASTIS.get('local', 'local_data_path'))
+    generation_mode = CONFIG_PASTIS.get('RST', 'generation_mode')
+    if generation_mode == 'intensity':
+        run_matrix = MatrixIntensityRST(initial_path=CONFIG_PASTIS.get('local', 'local_data_path'))
+    elif generation_mode == 'E_field':
+        run_matrix = MatrixEfieldRST(initial_path=CONFIG_PASTIS.get('local', 'local_data_path'))
+    else:
+        error_msg = f"generation_mode from config_pastis.ini is {generation_mode}, not a valid name!"
+        log.error(error_msg)
+        raise ValueError(error_msg)
     run_matrix.calc()
     dir_run = run_matrix.overall_dir
 
