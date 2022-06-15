39e3bac03386e4126761efada5728dc6a7253d66
Iva Laginja <iva.laginja@obspm.fr>
2022-06-15 22:38:30 +0200

    add correct file names; calculate num_seg and flat-to-flat diam; add robustness pararmeter

--- pastis/e2e_simulators/scda_telescopes.py
+++ pastis/e2e_simulators/scda_telescopes.py
@@ -65,36 +65,58 @@ class HexRingAPLC(ScdaAPLC):
     Parameters:
     ----------
     input_dir : string
-        Path to input files: apodizer, aperture, indexed aperture, Lyot stop.
+        Path to directories sorted by number of rings that contain the input files: apodizer, aperture, indexed aperture, Lyot stop.
     num_rings : integer
         Numer of segment rings.
     sampling : float
         Desired image plane sampling of coronagraphic PSF in pixels per lambda/D.
+    robustness_px : int
+        Robustness to Lyot stop misalignments in pixels. None, 2 or 4.
     """
-    def __init__(self, input_dir, num_rings, sampling):
-        # Diameter for each hex ring solution as provided by GSFC
-        # Taken from https://github.com/spacetelescope/aplc_optimization/blob/6bd5a6ecf46a3cf758c23853367d5b18e6a5a1d7/optimization_launchers/SCDA/do_LUVex_survey.py#L67
-        pupil_diameter = {1: 7.9445, 2:7.2617, 3:7.7231, 4:7.1522, 5:5.9941}
+    def __init__(self, input_dir, num_rings, sampling, robustness_px=None):
         self.num_rings = num_rings
-        diameter = pupil_diameter[num_rings]
+        data_in_repo = os.path.join(input_dir, f'{num_rings}-Hex')
 
-        #TODO: read these from input file headers?
+        # Diameter for each hex ring solution in meters as provided by GSFC
+        # Taken from https://github.com/spacetelescope/aplc_optimization/blob/6bd5a6ecf46a3cf758c23853367d5b18e6a5a1d7/optimization_launchers/SCDA/do_LUVex_survey.py#L67
+        pupil_diameter_circumscribed = {1: 7.9445, 2: 7.2617, 3: 7.7231, 4: 7.1522, 5: 5.9941}
+        pupil_diameter_inscribed = {1: 6.0023, 2: 5.9994, 3: 5.9899, 4: 5.9937, 5: 6.8526}
+        diameter_circumscribed = pupil_diameter_circumscribed[num_rings]
+        diameter_inscribed = pupil_diameter_inscribed[num_rings]
+
+        num_seg = 3 * num_rings * (num_rings + 1) + 1
+        gap_size = 0.06    # in meters
+        if num_rings in [1, 2]:
+            seg_flat_to_flat = (diameter_inscribed - 2 * (num_rings - 1) * gap_size) / (2 * (num_rings - 1) + 1)
+        elif num_rings in [3, 4, 5]:
+            seg_flat_to_flat = (diameter_circumscribed - 2 * num_rings * gap_size) / (2 * num_rings + 1)
+        else:
+            raise ValueError(f"No telescope/coronagraph solution provided for {num_rings} number of rings.")
+
+        if robustness_px is None:
+            robust = 0
+        elif robustness_px == 2:
+            robust = 1
+        elif robustness_px == 4:
+            robust = 2
+        else:
+            raise ValueError(f"An apodizer design with robustness to a LS misalignment of {robustness_px} pixels does not exist for this aperture.")
+
+        # These parameters are contained in the fits header of the apodizer
         pxsize = 1024
-        fpm_rad =  3.5
+        fpm_rad = 3.5
         fpm_px = 150
         iwa = 3.4
         owa = 12
         imlamD = 1.2 * owa
 
-        num_seg = None   #TODO from num_rings
-        if num_seg in [1,2]:
-            aper_fname = f'TelAp_LUVex_{num_seg:02d}-Hex_gy_ovsamp04__N{pxsize:04d}.fits'
-        else:
-            aper_fname = f'TelAp_LUVex_{num_seg:02d}-Hex_gy_clipped_ovsamp04__N{pxsize:04d}.fits'
+        if num_rings in [1, 2]:
+            aper_fname = f'masks/TelAp_LUVex_{num_rings:02d}-Hex_gy_ovsamp04__N{pxsize:04d}.fits'
+        elif num_rings in [3, 4, 5]:
+            aper_fname = f'masks/TelAp_LUVex_{num_rings:02d}-Hex_gy_clipped_ovsamp04__N{pxsize:04d}.fits'
         aper_ind_fname = aper_fname.split('.')[0] + '_indexed.fits'
-        apod_fname = None   #TODO
-        ls_fname = f'LS_LUVex_{num_rings:02d}-Hex_ID0000_OD0982_no_struts_gy_ovsamp04_N{pxsize:04d}.fits'
-        seg_flat_to_flat = None   #TODO from num_rings, maybe dict if corners cut off
+        apod_fname = f'solutions/{robust}_SCDA_N1024_FPM350M0150_IWA0340_OWA01200_C10_BW10_Nlam3_LS_IDex_ID_OD0_OD_ls_982_no_strut.fits'
+        ls_fname = f'masks/LS_LUVex_{num_rings:02d}-Hex_ID0000_OD0982_no_struts_gy_ovsamp04_N{pxsize:04d}.fits'
 
         aplc_params = {'num_seg': num_seg,
                        'aper_fname': aper_fname,
@@ -107,5 +129,5 @@ class HexRingAPLC(ScdaAPLC):
                        'iwa': iwa,
                        'owa': owa}
 
-        super().__init__(input_dir=input_dir, sampling=sampling, diameter=diameter, seg_flat_to_flat=seg_flat_to_flat,
-                         wvln=1, imlamD=imlamD, aplc_params=aplc_params)
+        super().__init__(input_dir=data_in_repo, sampling=sampling, diameter=diameter_circumscribed,
+                         seg_flat_to_flat=seg_flat_to_flat, wvln=1, imlamD=imlamD, aplc_params=aplc_params)
