a03a359cde2253913fabc5c405d734855c059509
Ananya Sahoo <ananyasahoo1904@gmail.com>
2022-10-17 18:11:47 -0400

    re-arrange code block to be compatible with zernike mirror

--- pastis/launchers/run_luvoir_efields.py
+++ pastis/launchers/run_luvoir_efields.py
@@ -11,18 +11,43 @@ import pastis.plotting as ppl
 if __name__ == '__main__':
 
     APLC_DESIGN = 'small'
-    DM = 'harris_seg_mirror'   # Possible: "seg_mirror", "harris_seg_mirror", "zernike_mirror"
 
-    # Needed for Harris mirror
-    fpath = CONFIG_PASTIS.get('LUVOIR', 'harris_data_path')  # path to Harris spreadsheet
-    pad_orientations = np.pi / 2 * np.ones(120)
+    # Instantiate the LUVOIR telescope
+    optics_input = os.path.join(util.find_repo_location(), CONFIG_PASTIS.get('LUVOIR', 'optics_path_in_repo'))
+    coronagraph_design = CONFIG_PASTIS.get('LUVOIR', 'coronagraph_design')
+    sampling = CONFIG_PASTIS.getfloat('LUVOIR', 'sampling')
+    tel = LuvoirA_APLC(optics_input, coronagraph_design, sampling)
+    nm_aber = CONFIG_PASTIS.getfloat('LUVOIR', 'calibration_aberration') * 1e-9
 
-    DM_SPEC = (fpath, pad_orientations, True, False, False)
+    # -----------------------------
     # DM_SPEC = tuple or int, specification for the used DM -
-    #    for seg_mirror: int, number of local Zernike modes on each segment
-    #    for harris_seg_mirror: tuple (string, array, bool, bool, bool),
-    #    absolute path to Harris spreadsheet, pad orientations, choice of Harris mode sets (thermal, mechanical, other)
-    #    for zernike_mirror: int, number of global Zernikes
+    # for seg_mirror: int, number of local Zernike modes on each segment
+    # for harris_seg_mirror: tuple (string, array, bool, bool, bool),
+    # absolute path to Harris spreadsheet, pad orientations, choice of Harris mode sets (thermal, mechanical, other)
+    # for zernike_mirror: int, number of global Zernikes
+
+    # If Harris deformable mirror, uncomment the following lines
+    # --------------------------------------------------------------------------------------------#
+    # DM = 'harris_seg_mirror'  # Possible: "seg_mirror", "harris_seg_mirror", "zernike_mirror"
+    # fpath = CONFIG_PASTIS.get('LUVOIR', 'harris_data_path')  # path to Harris spreadsheet
+    # pad_orientations = np.pi / 2 * np.ones(CONFIG_PASTIS.getint('LUVOIR', 'nb_subapertures'))
+    # DM_SPEC = (fpath, pad_orientations, True, False, False)
+    # pad_orientation = np.pi / 2 * np.ones(tel.nseg)
+    # filepath = CONFIG_PASTIS.get('LUVOIR', 'harris_data_path')
+    # tel.create_segmented_harris_mirror(filepath, pad_orientation, thermal=True, mechanical=False, other=False)
+    # tel.harris_sm
+    # num_actuators = tel.harris_sm.num_actuators
+    # num_modes = 5
+    # tel.harris_sm.flatten()
+
+    # If Segmented Zernike Mirror, uncomment the following lines
+    # --------------------------------------------------------------------------------------------#
+    DM = 'seg_mirror'  # Possible: "seg_mirror", "harris_seg_mirror", "zernike_mirror"
+    DM_SPEC = 5
+    tel.create_segmented_mirror(DM_SPEC)
+    num_modes = DM_SPEC
+    num_actuators = tel.sm.num_actuators
+    tel.sm.flatten()
 
     # First generate a couple of matrices
     run_matrix = MatrixEfieldLuvoirA(which_dm=DM, dm_spec=DM_SPEC, design=APLC_DESIGN,
@@ -33,23 +58,6 @@ if __name__ == '__main__':
     dir_run = run_matrix.overall_dir
     print(f'All saved to {dir_run}.')
 
-    # Instantiate the LUVOIR telescope
-    optics_input = os.path.join(util.find_repo_location(), CONFIG_PASTIS.get('LUVOIR', 'optics_path_in_repo'))
-    coronagraph_design = CONFIG_PASTIS.get('LUVOIR', 'coronagraph_design')
-    sampling = CONFIG_PASTIS.getfloat('LUVOIR', 'sampling')
-    tel = LuvoirA_APLC(optics_input, coronagraph_design, sampling)
-    nm_aber = CONFIG_PASTIS.getfloat('LUVOIR', 'calibration_aberration') * 1e-9
-
-    # Create harris deformable mirror
-    tel.create_segmented_harris_mirror(fpath, pad_orientations, thermal=True, mechanical=False, other=False)
-    tel.harris_sm
-
-    # get number of poking modes
-    num_actuators = tel.harris_sm.num_actuators
-    num_modes = 5
-
-    # calculate dark hole contrast
-    tel.harris_sm.flatten()
     unaberrated_coro_psf, ref = tel.calc_psf(ref=True, display_intermediate=False, norm_one_photon=True)
     norm = np.max(ref)
     dh_intensity = (unaberrated_coro_psf / norm) * tel.dh_mask
